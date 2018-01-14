3d7ae241f9b39d315ed798e5ef747f9db934c380
zzzzBov <zzzzBov@gmail.com>
2018-01-14 17:29:08 -0500

    Build: normalized error text in skip()

--- skip.js
+++ skip.js
@@ -1,8 +1,8 @@
 module.exports = count => {
   if (isNaN(count)) {
-    throw new TypeError(`"count" must be a number.`)
+    throw new TypeError(`"count" parameter must be a number.`)
   } else if (count < 0) {
-    throw new RangeError(`"count" must not be negative.`)
+    throw new RangeError(`"count" parameter must not be negative.`)
   } else {
     return function * (gen) {
       let i = 0
