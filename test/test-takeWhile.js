0a8a1505e8dc7b12b05df0cf9ef559f1ef9768aa
zzzzBov <zzzzBov@gmail.com>
2018-01-14 17:42:25 -0500

    Tests: created tests for takeWhile()

---
+++ test/test-takeWhile.js
@@ -0,0 +1,38 @@
+/* eslint-env mocha */
+
+const {expect} = require('chai')
+const takeWhile = require('../takeWhile')
+
+describe(`gen`, () => {
+  describe(`takeWhile(test)`, () => {
+    it(`should be a function`, () => {
+      expect(takeWhile).to.be.a('function')
+    })
+
+    it(`should produce a function when passed a test function`, () => {
+      const result = takeWhile(() => {})
+
+      expect(result).to.be.a('function')
+    })
+
+    it(`should throw a "TypeError" if not provided a test function`, () => {
+      const noInput = () => {
+        takeWhile()
+      }
+
+      expect(noInput).to.throw(TypeError)
+
+      const badInput = () => {
+        takeWhile(1)
+      }
+
+      expect(badInput).to.throw(TypeError)
+    })
+
+    it(`should take until the test function returns a falsey value`, () => {
+      const result = [...takeWhile(x => x < 4)([1, 2, 3, 4, 5])]
+
+      expect(result).to.deep.equal([1, 2, 3])
+    })
+  })
+})
