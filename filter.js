c7f4d0de1c4adf763b67166c3678bb6ba412c886
zzzzBov <zzzzBov@gmail.com>
2018-01-14 16:09:30 -0500

    Build: implemented filter()

---
+++ filter.js
@@ -0,0 +1,14 @@
+module.exports = test => {
+  if (test instanceof Function) {
+    return function* (gen) {
+      let i = 0
+      for (const value of gen) {
+        if (test(value, i++)) {
+          yield value
+        }
+      }
+    }
+  } else {
+    throw new TypeError(`"test" parameter must be a function.`)
+  }
+}
