e43ef302b14e82a54f51e2756c6e9283330afbf8
zzzzBov <zzzzBov@gmail.com>
2018-01-20 13:50:11 -0500

    Tests:
    created tests for append()

---
+++ test/test-append.js
@@ -0,0 +1,24 @@
+/* eslint-env mocha */
+
+const {expect} = require('chai')
+const append = require('../append')
+
+describe(`gen`, () => {
+  describe(`append(...items)`, () => {
+    it(`should be a function`, () => {
+      expect(append).to.be.a('function')
+    })
+
+    it(`should produce a function when passed some items`, () => {
+      const result = append(1)
+
+      expect(result).to.be.a('function')
+    })
+
+    it(`should append items to the provided collection`, () => {
+      const result = [...append(4, 5, 6)([1, 2, 3])]
+
+      expect(result).to.deep.equal([1, 2, 3, 4, 5, 6])
+    })
+  })
+})
