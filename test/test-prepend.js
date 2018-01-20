e3fea82f2556ef39f10216afa75bb9207b07805c
zzzzBov <zzzzBov@gmail.com>
2018-01-20 13:56:45 -0500

    Tests:
    created tests for prepend()

---
+++ test/test-prepend.js
@@ -0,0 +1,24 @@
+/* eslint-env mocha */
+
+const {expect} = require('chai')
+const prepend = require('../prepend')
+
+describe(`gen`, () => {
+  describe(`prepend(...items)`, () => {
+    it(`should be a function`, () => {
+      expect(prepend).to.be.a('function')
+    })
+
+    it(`should produce a function when passed some items`, () => {
+      const result = prepend(1)
+
+      expect(result).to.be.a('function')
+    })
+
+    it(`should prepend items to the provided collection`, () => {
+      const result = [...prepend(1, 2, 3)([4, 5, 6])]
+
+      expect(result).to.deep.equal([1, 2, 3, 4, 5, 6])
+    })
+  })
+})
