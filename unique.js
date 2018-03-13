d3c34b82508fd58857a1785be9972bbd51965128
zzzzBov <zzzzBov@gmail.com>
2018-03-13 17:20:02 -0400

    Build:
    implemented `unique`

--- unique.js
+++ unique.js
@@ -1,17 +1,3 @@
-const all = require('./all')
-
-module.exports = (comparator = (a, b) => a === b) => {
-  if (comparator instanceof Function) {
-    return function * (gen) {
-      const values = []
-      for (const value of gen) {
-        if (all(v => !comparator(v, value))(values)) {
-          values.push(value)
-          yield value
-        }
-      }
-    }
-  } else {
-    throw new TypeError(`"comparator" parameter must be a function.`)
-  }
+module.exports = function * (gen) {
+  yield * new Set(gen)
 }
