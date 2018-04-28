27bb9e66f70d0cbc7ece37f5d385cf287ebc2af8
zzzzBov <zzzzBov@gmail.com>
2018-04-28 15:18:12 -0400

    Tests:
    added test for the index list of all gen functions

---
+++ test/test-index.js
@@ -0,0 +1,31 @@
+/* eslint-env mocha */
+
+const {expect} = require('chai')
+const gen = require('../index')
+
+describe(`gen index`, () => {
+  ;[
+    'all',
+    'any',
+    'append',
+    'concat',
+    'filter',
+    'integers',
+    'map',
+    'mapMany',
+    'pipe',
+    'prepend',
+    'reduce',
+    'skip',
+    'skipWhile',
+    'take',
+    'takeWhile',
+    'unique'
+  ].forEach(fn => {
+    describe(fn, () => {
+      it(`${fn} should be a function`, () => {
+        expect(gen[fn]).to.be.a('function')
+      })
+    })
+  })
+})
