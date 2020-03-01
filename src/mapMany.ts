282b7b83a3fdadbd4e752a9d7692894fb3a96eef
Tim Leverett <zzzzbov@gmail.com>
2020-03-01 18:02:36 -0500

    ported `mapMany` to TS

---
+++ src/mapMany.ts
@@ -0,0 +1,10 @@
+import { ITransform } from './ITransform'
+
+export const mapMany = <T, U = T>(transform: ITransform<T, Iterable<U>>) => (
+  function * (iterable: Iterable<T>) {
+    let i = 0
+    for (const value of iterable) {
+      yield * transform(value, i++)
+    }
+  }
+)
