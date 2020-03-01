6d2bdbf98f36f845d651055dc87cce2d14d0ccb0
Tim Leverett <zzzzbov@gmail.com>
2020-03-01 17:35:11 -0500

    ported `count` to TS

---
+++ src/count.test.ts
@@ -0,0 +1,17 @@
+import { count } from './count'
+
+describe(`count(iterable)`, () => {
+  it.each`
+    input           | expected
+    ${[]}           | ${0}
+    ${[1]}          | ${1}
+    ${[1, 2, 3, 4]} | ${4}
+  `(`should count $expected items`, ({
+    input,
+    expected
+  }) => {
+    const output = count(input)
+
+    expect(output).toBe(expected)
+  })
+})
