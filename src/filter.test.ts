ceb759c5a72bd1ae10c92a5347246ff1610a1b25
Tim Leverett <zzzzbov@gmail.com>
2020-03-01 17:40:27 -0500

    ported `filter` to TS

---
+++ src/filter.test.ts
@@ -0,0 +1,9 @@
+import { filter } from './filter'
+
+describe(`filter(predicate)(iterable)`, () => {
+  it(`should filter out values that don't match the provided predicate`, () => {
+    const output = [...filter<number>(x => x % 2)([1, 2, 3, 4, 5])]
+
+    expect(output).toEqual([1, 3, 5])
+  })
+})
---
+++ src/filter.ts
@@ -0,0 +1,10 @@
+import { IPredicate } from './IPredicate';
+
+export const filter = <T>(predicate: IPredicate<T>) => function * (iterable: Iterable<T>) {
+  let i = 0
+  for (const value of iterable) {
+    if (predicate(value, i++)) {
+      yield value
+    }
+  }
+}
