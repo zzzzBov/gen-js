dc26221fc8476761ce00fcef0f4fd6762e7f4d86
Tim Leverett <zzzzbov@gmail.com>
2020-03-06 09:23:25 -0500

    ported `takeWhile` to TS

---
+++ src/takeWhile.ts
@@ -0,0 +1,14 @@
+import { IPredicate } from './IPredicate'
+
+export const takeWhile = <T>(predicate: IPredicate<T>) => (
+  function * (iterable: Iterable<T>) {
+    let i = 0
+    for (const value of iterable) {
+      if (predicate(value, i++)) {
+        yield value
+      } else {
+        break
+      }
+    }
+  }
+)
